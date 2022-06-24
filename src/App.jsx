8f0185033f65b51fb82be0db66d6db324813806e
Romano <romanomanfredmantek@gmail.com>
2022-06-24 10:36:43 +0200

    create addStudent method

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ class App extends React.Component {
     this.handleButtonClicked = this.handleButtonClicked.bind(this);
     this.addAssignment = this.addAssignment.bind(this);
     /*Uncomment the line below to bind the method*/
-    /*this.addStudent = this.addStudent.bind(this);*/
+    this.addStudent = this.addStudent.bind(this);
     this.addGrade = this.addGrade.bind(this);
   }
 
@@ -33,8 +33,12 @@ class App extends React.Component {
     });
   }
 
-  /*Write an addStudent method here*/
 
+  addStudent(studentName) {
+    this.setState({
+      student: this.state.student.concat(studentName)
+    });
+  }
   addGrade(assignment, student, score) {
     let grades = this.state.grades;
     let assignmentName = assignment;
